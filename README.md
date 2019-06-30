b176b9a2ff37358d5aa91b168baf6a3ef5a6c0fc
afeiship <1290657123@qq.com>
2019-06-30 21:53:59 +0800

    add: intial - vb-button

---
+++ README.md
@@ -0,0 +1,6 @@
+# vue-bulma
+> Bulma for vue.
+
+
+## resources
+- https://juejin.im/post/5afcd516f265da0b9e65414b
---
+++ src/components/button/index.vue
@@ -0,0 +1,10 @@
+<template>
+  <button class="vb-button">
+    <slot></slot>
+  </button>
+</template>
+<script>
+export default {
+  name: 'vb-button'
+}
+</script>
