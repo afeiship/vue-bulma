cf67e591f46997d19e49af0c336d46b810b38315
afeiship <1290657123@qq.com>
2019-07-13 23:04:04 +0800

    add: icon

---
+++ src/components/icon.vue
@@ -0,0 +1,19 @@
+<template>
+  <section class="page-box">
+    <vb-box>
+      <vb-icon color="success" size="large">
+        <i class="ion ion-ios-alarm" />
+      </vb-icon>
+
+      <vb-icon color="primary" size="large">
+        <i class="ion ion-ios-aperture" />
+      </vb-icon>
+    </vb-box>
+  </section>
+</template>
+
+<script>
+export default {
+  name: 'page-box'
+};
+</script>
---
+++ src/packages/icon/index.vue
@@ -0,0 +1,18 @@
+<template>
+  <span
+    class="icon"
+    :class="[color ? `has-text-${color}` : '', size ? `is-${size}` : '']"
+  >
+    <slot></slot>
+  </span>
+</template>
+
+<script>
+export default {
+  name: 'vb-icon',
+  props: {
+    color: String,
+    size: String
+  }
+};
+</script>
