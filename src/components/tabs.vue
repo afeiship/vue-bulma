e9bd7fb88039bbd21ff1698ae2aadd7b1d124676
afeiship <1290657123@qq.com>
2019-07-14 21:04:15 +0800

    add: basic tabs

---
+++ src/components/tabs.vue
@@ -0,0 +1,16 @@
+<template>
+  <section class="page-box">
+    <vb-tabs :items="items" />
+  </section>
+</template>
+
+<script>
+export default {
+  name: 'page-tabs',
+  data() {
+    return {
+      items: ['Music', 'Video', 'News']
+    };
+  }
+};
+</script>
---
+++ src/packages/tabs/index.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="tabs">
+    <ul>
+      <li
+        v-for="(item, index) in items"
+        :key="index"
+        :class="[{ 'is-active': activeIndex === index }]"
+        @click="change(index)"
+      >
+        <a>
+          <vb-icon v-if="item.icon" size="small">{{ item.icon }}</vb-icon>
+          <span>{{ item.text || item }}</span>
+        </a>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'vb-tabs',
+  props: {
+    items: Array,
+    activeIndex: {
+      type: Number,
+      default: 0
+    }
+  },
+  methods: {
+    change(inIndex) {
+      this.$emit('update:activeIndex', inIndex);
+    }
+  }
+};
+</script>
