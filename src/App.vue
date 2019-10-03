383b00abd6cb3a7bce1a76d382f613a6def4d37c
afeiship <1290657123@qq.com>
2019-10-03 23:06:44 +0800

    add: beatify sidebar

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,15 @@
 <template>
   <div id="app">
     <aside class="page-sider">
-      <h2>Copmonents</h2>
+      <h2>
+        <img src="https://bulma.io/images/bulma-logo.png" />
+      </h2>
       <ul>
         <li v-for="item in components" :key="item">
-          <router-link :to="`/${item}`">{{ item }}</router-link>
+          <router-link :to="`/${item}`">
+            <i class="ion ion-ios-menu"></i>
+            <span>{{ item }}</span>
+          </router-link>
         </li>
       </ul>
     </aside>
